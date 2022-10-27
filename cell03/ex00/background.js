dc59ee755bde5de2723e96b53365488c346a71d8
BekxFR <104771705+BekxFR@users.noreply.github.com>
2022-10-27 16:39:19 +0200

    day4

---
+++ cell03/ex00/background.js
@@ -0,0 +1,15 @@
+function func()
+{
+		document.body.style.backgroundColor = getRandomColor();
+}
+
+const btn = document.getElementById('butn');
+btn.addEventListener('click', function onClick() {
+	butn.style.backgroundColor = getRandomColor();
+});
+
+document.getElementById('butn').style.backgroundColor = getRandomColor();
+
+function getRandomColor() {
+	return '#'+Math.floor(Math.random()*0xFFFFFFFF).toString(16);
+}
